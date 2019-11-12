0cc2ea98c3f21609904ba614b3289fa2ab8d70e0
Luke Brannigan <lukebrannigan57@gmail.com>
2019-11-12 15:32:34 -0500

    added some code and basic templating

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [artists, setInputValue] = React.useState("");
+
+
+  const [artistCard, setArtistCards] = React.useState([
+    ""
+  ]);
+
+  function handleClick() {
+    setArtistCards([artists, ...artistCard]);
+    
+  }
+
+  const [fetchResponse, setFetchResponse] = React.useState({});
+
+
+  React.useEffect(() => {
+    fetch("http://localhost:8080/artists")
+      .then(res => res.json())
+      .then(response => {
+        console.log(response);
+        setFetchResponse(response);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <h1>Biscuit Time!</h1>
+        <button className="artists-button" onClick={handleClick}>Artists</button>
+
       </header>
+      <output>
+        {fetchResponse.artist && fetchResponse.artist[0].name}
+      </output>
+      <div className="artist-cards">artist cards
+        <div className="artist-card">artist card
+          <h2 className="artist-name">artist name</h2>
+          <img src="../public/logo"></img>
+          <h3 className="record-label">record label</h3>
+          <h3 className="hometown">hometown</h3>
+        </div>
+      </div>
     </div>
   );
 }
+
 
 export default App;
