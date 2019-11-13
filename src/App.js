e2cf4ad1fdf2364fd6cb86af1e80232f57487107
Luke Brannigan <lukebrannigan57@gmail.com>
2019-11-13 13:16:45 -0500

    working on artist

--- src/App.js
+++ src/App.js
@@ -1,68 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component {
+import Artists from './components/artists'
+
+class App extends Component{
   
   state = {
     artists: []
-}
+};
 
 componentDidMount() {
   fetch("http://localhost:8080/artists")
   .then(res => res.json())
   .then((data) => {
-    this.setState({contacts: data })
+    this.setState({artists: data })
   })
   .catch(console.log)
-}
+};
 
-
-
-  // const [artists, setInputValue] = React.useState("");
-
-
-  // const [artistCard, setArtistCards] = React.useState([
-  //   ""
-  // ]);
-
-  // function handleClick() {
-  //   setArtistCards([artists, ...artistCard]);
-    
-  // }
-
-  // const [fetchResponse, setFetchResponse] = React.useState({});
-
-
-  // React.useEffect(() => {
-  //   fetch("http://localhost:8080/artists")
-  //     .then(res => res.json())
-  //     .then(response => {
-  //       console.log(response);
-  //       setFetchResponse(response);
-  //     });
-  // }, []);
-
+render() {
   return (
-    <div className="App">
-      <header className="header">
-        <h1>Biscuit Time!</h1>
-        <button className="artists-button" onClick={handleClick}>Artists</button>
-
-      </header>
-      <output>
-        {fetchResponse.artist && fetchResponse.artist[0].name}
-      </output>
-      <div className="artist-cards">artist cards
-        <div className="artist-card">artist card
-          <h2 className="artist-name">artist name</h2>
-          <img src="../public/logo"></img>
-          <h3 className="record-label">record label</h3>
-          <h3 className="hometown">hometown</h3>
-        </div>
-      </div>
-    </div>
-  );
+    <Artists artists={this.state.artists}/>
+  )
+};
 }
+
+
+
 
 
 export default App;
---
+++ src/components/artists.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Artists = ({ artists }) => {
+    return(
+    {artists.map((artist) => 
+    <div className="artist-cards">artist cards
+         <div className="artist-card">artist card
+           <h2 className="artist-name">{artist.name}</h2>
+           <img src={artist.imageUrl} alt={artist.name}></img>
+           <h3 className="record-label">{artist.recordLabel}</h3>
+           <h3 className="hometown">{artist.homeTown}</h3>
+         </div>
+       </div>
+       )})
+  }
+
+  export default Artists
