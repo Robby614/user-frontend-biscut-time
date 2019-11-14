90bdd6368e5e535ca495c0ba5e256b74df386970
Luke Brannigan <lukebrannigan57@gmail.com>
2019-11-14 13:50:01 -0500

    styling changes

--- src/App.js
+++ src/App.js
@@ -4,23 +4,35 @@ import './App.css';
 import Artists from './components/artists'
 
 class App extends Component{
+
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     showArtists: false,
+  //   }
+  //   this.renderArtists = this.renderArtists.bind(this)
+  // }
+
+  // renderArtists() {
+  //   this.setState({
+  //     showArtists:true,
+  //   })
+  // }
+
   render() {
   return (
     <div className="App">
       <header className="header">
         <h1>Biscuit Time!</h1>
-        <button className="artists-button" onClick={handleClick}>Artists</button>
+      <button className="artists-button" onClick={this.renderArtists}>Artists</button>
+        
      </header>
-     <Artists artists={this.state.artists}/>
+      <div>
+        <Artists artists={this.state.artists}/>
+      </div>
 
    </div>
   )}
- 
-  // render() {
-  //   return (
-  //     <Artists artists={this.state.artists}/>
-  //   )
-  // };
   
   state = {
     artists: []
@@ -30,16 +42,13 @@ componentDidMount() {
   fetch("http://localhost:8080/api/artists")
   .then(res => res.json())
   .then((data) => {
-    this.setState({artists: data })
+    this.setState({ artists: data })
   })
   .catch(console.log)
 };
 }
-function handleClick(){
-  console.log('click')
-
-}
 
 
+// []
 
 export default App;
--- src/components/artists.js
+++ src/components/artists.js
@@ -2,17 +2,16 @@ import React from 'react'
 
 const Artists = ({ artists }) => {
     return(
-        <div>
-            <center><h1> Artists</h1></center>
+        <div className='artist-cards'>
+            
             {artists.map((artist) => 
-                <div className="artist-cards">
                     <div className="artist-card">artist card
                         <h2 className="artist-name">{artist.name}</h2>
                         <img src={artist.imageUrl} alt={artist.name}></img>
                         <h3 className="record-label">{artist.recordLabel}</h3>
                         <h3 className="hometown">{artist.homeTown}</h3>
                     </div>
-                </div>
+            
             )}
        </div>
     )}
